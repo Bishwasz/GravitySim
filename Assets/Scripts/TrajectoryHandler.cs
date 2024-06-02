61408b3e2928cd2172c68b0e21ec21ad47884cfc
kiran079 <kiran79bh@gmail.com>
2024-06-01 21:53:59 -0500

    added drag initial velocity and trajectory

---
+++ Assets/Scripts/TrajectoryHandler.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryHandler : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        ConfigureLineRenderer();
+    }
+
+    private void ConfigureLineRenderer()
+    {
+        lineRenderer.positionCount = 0;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+        lineRenderer.startColor = new Color(1, 1, 0, 1); // yellow
+        lineRenderer.endColor = new Color(1, 1, 1, 0); // Fades to transparent
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+    }
+
+    public void DrawTrajectory(Vector2 startPosition, float G, Vector2 initialVelocity, List<CBody> bodies, float timeStep, int steps)
+    {
+        lineRenderer.positionCount = steps;
+        Vector2 currentPos = startPosition;
+        Vector2 currentVel = initialVelocity;
+
+        for (int i = 0; i < steps; i++)
+        {
+            Vector2 acceleration = Vector2.zero;
+
+            foreach (var other in bodies)
+            {
+                Vector2 direction = other.GetPosition() - currentPos;
+                float distance = direction.magnitude;
+                // TODO: handle collisions
+
+                float accMagnitude = G * other.mass / (distance * distance);
+                Vector2 acc = direction.normalized * accMagnitude;
+                acceleration += acc;
+            }
+
+            currentVel += acceleration * timeStep;
+            currentPos += currentVel * timeStep;
+            lineRenderer.SetPosition(i, currentPos);
+        }
+    }
+
+    public void ClearTrajectory()
+    {
+        lineRenderer.positionCount = 0;
+    }
+
+    public void EnableTrajectory()
+    {
+        lineRenderer.enabled = true;
+    }
+
+    public void DisableTrajectory()
+    {
+        lineRenderer.enabled = false;
+    }
+}
